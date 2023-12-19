c2759e44972d5c9cead5a35571faab8d59b8493b
AscensaoJ <123513572+AscensaoJ@users.noreply.github.com>
2023-12-18 18:48:24 -0800

    Update apiAuth.jsx

--- frontend/src/api/apiAuth.jsx
+++ frontend/src/api/apiAuth.jsx
@@ -1,80 +1,118 @@
 import { encode } from "base64-arraybuffer";
 const { subtle } = globalThis.crypto;
+const port = 3000;
 
 // Login user and retrieve stats from database
 export async function login(user, pass) {
-  try {
-    const bib = await encrypter(pass);
-    const response = await fetch(`http://localhost:3000/api/login`, {
-      method: "POST",
-      body: JSON.stringify({
-        user: user,
-        hay: bib
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const result = await response.json();
-    
-    localStorage.setItem('quizappjwt', result.jwt);
-    return {
-      username: result.username,
-      questions: result.questions,
-      correct: result.correct
+  const encrypted = await encrypter(pass);
+  const response = await fetch(`http://localhost:${port}/api/login`, {
+    method: "POST",
+    body: JSON.stringify({
+      user: user,
+      hay: encrypted
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
-  }
-  catch (err) {
-    console.log(err);
-  }
+  });
+  const result = await response.json();
+  
+  localStorage.setItem('quizappjwt', result.jwt);
+  sessionStorage.setItem('quizapptotal', result.questions);
+  sessionStorage.setItem('quizappcorrect', result.correct);
+  sessionStorage.setItem('quizappuser', result.username);
 }
 
 // Register new user to database
 export async function register(user, pass) {
-  try {
-    const bib = await encrypter(pass);
-    const response = await fetch(`http://localhost:3000/api/register`, {
-      method: "POST",
-      body: JSON.stringify({
-        user: user,
-        hay: bib
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const result = await response.json();
-    
-    localStorage.setItem('quizappjwt', result.jwt);
-    return {
-      username: result.username,
-      questions: result.questions,
-      correct: result.correct
+  const encrypted = await encrypter(pass);
+  const response = await fetch(`http://localhost:${port}/api/register`, {
+    method: "POST",
+    body: JSON.stringify({
+      user: user,
+      hay: encrypted
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
-  }
-  catch (err) {
-    console.log(err);
-  }
+  });
+  const result = await response.json();
+  
+  localStorage.setItem('quizappjwt', result.jwt);
+  sessionStorage.setItem('quizapptotal', result.questions);
+  sessionStorage.setItem('quizappcorrect', result.correct);
+  sessionStorage.setItem('quizappuser', result.username);
 }
 
 // Handle local storage logout
 export function logout() {
   if(confirm('Do you want to log out?')){
     localStorage.removeItem('quizappjwt');
+    sessionStorage.setItem('quizapptotal', 0);
+    sessionStorage.setItem('quizappcorrect', 0);
+    sessionStorage.setItem('quizappuser', 'Guest');
     return true;
+  }
+}
+
+// Gets user data from database
+export async function loadStats() {
+  try {
+    const jwt = localStorage.getItem('quizappjwt');
+    const response = await fetch(`http://localhost:${port}/api/getStats`, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + jwt
+      }
+    });
+    const result = await response.json();
+    sessionStorage.setItem('quizapptotal', result.questions);
+    sessionStorage.setItem('quizappcorrect', result.correct);
+    sessionStorage.setItem('quizappuser', result.user);
+    return true;
+  } catch (err) {
+    console.error(err);
   }
 }
 
 // Checks to see if user exists in database
 export async function checkUser(user) {
+  let userCheck = await fetch(`http://localhost:${port}/api/checkUser`, {
+    method: "POST",
+    body: JSON.stringify({
+      user: user
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  const data = await userCheck.json();
+  console.log(data)
+  switch (data.result) {
+    case true:
+      return true;
+    case false:
+      return false;
+    case null:
+      alert(data.message);
+      return null;
+    default:
+      alert('Check incomplete');
+      break;
+  }
+}
+
+// Checks if JWT is valid and if user ID, uid, exists in database without deleted flag set
+export async function checkJWT(user) {
   try {
-    let userCheck = await fetch(`http://localhost:3000/api/checkUser`, {
-      method: "POST",
-      body: JSON.stringify({
-        user: user
-      }),
+    const jwt = localStorage.getItem('quizappjwt');
+    let userCheck = await fetch(`http://localhost:${port}/api/checkJWT`, {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + jwt
       }
     });
 
@@ -94,15 +132,38 @@ export async function checkUser(user) {
     }
   }
   catch (err) {
-    console.log(err);
+    console.error(err);
   }
 }
-      
+
+// Asks database to set deleted flag to 1 for given uid in JWT. Logs out user locally
+export async function deleteUser() {
+    if(confirm('Are you sure you want to delete your account?')) {
+      try {
+        const jwt = localStorage.getItem('quizappjwt');
+        const deleted = fetch(`http://localhost:${port}/api/deleteUser`, {
+          method: "DELETE",
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + jwt
+          }
+        });
+        localStorage.removeItem('quizappjwt');
+        sessionStorage.setItem('quizapptotal', 0);
+        sessionStorage.setItem('quizappcorrect', 0);
+        sessionStorage.setItem('quizappuser', 'Guest');
+        return true;
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      return false;
+    }
+}  
 
 // Ensure passwords entered for registration match
 export function checker(pw1, pw2) {
-  if (pw1 != pw2) {
-    //alert('Passwords do not match!');
+  if(pw1 != pw2) {
     return false;
   }
   return true;
@@ -110,54 +171,39 @@ export function checker(pw1, pw2) {
 
 // Update user data in database
 export async function updateUser(user, questions, correct) {
-  try {
-    const jwt = localStorage.getItem('quizappjwt');
-    const response = await fetch(`http://localhost:3000/api/updateScore`, {
-      method: "POST",
-      body: JSON.stringify({
-        user: user,
-        questions: questions,
-        correct: correct
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + jwt
-      }
-    });
-    const result = response.json();
-    console.log(result);
-  }
-  catch (err) {
-    console.log(err);
-  }
-}
-
-// Retrieve stats from database if jwt in local storage on load
-export async function loadStats() {
-  
+  const jwt = localStorage.getItem('quizappjwt');
+  const response = await fetch(`http://localhost:${port}/api/updateScore`, {
+    method: "PUT",
+    body: JSON.stringify({
+      user: user,
+      questions: questions,
+      correct: correct
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + jwt
+    }
+  });
+  const result = response.json();
+  console.log(result);
 }
 
 // Encrypt password for network transmission
 async function encrypter(pass) {
-  try {
-    const toEncrypt = str2ab(pass);
-    const response = await fetch('http://localhost:3000/api/getkey', {method: 'GET'});
-    const result = await response.json();
-    const key = await subtle.importKey('jwk', result.key, {name: 'RSA-OAEP', hash: 'SHA-512'}, true, [ 'encrypt' ]);
-    const encrypted = await subtle.encrypt({name: "RSA-OAEP"}, key, toEncrypt);
-    let toEncode = new DataView(encrypted);
-    let toSend = encode(toEncode.buffer);
-    return toSend;
-  }
-  catch (err) {
-    console.log(err);
-  }
+  const enc = str2ab(pass);
+  const response = await fetch('http://localhost:' + port + '/api/getkey', {method: 'GET'});
+  const result = await response.json();
+  const key = await subtle.importKey('jwk', result.key, {name: 'RSA-OAEP', hash: 'SHA-512'}, true, [ 'encrypt' ]);
+  const encrypted = await subtle.encrypt({name: "RSA-OAEP"}, key, enc);
+  let viewer = new DataView(encrypted);
+  let encoded = encode(viewer.buffer);
+  return encoded;
 }
-
-// Translate plaintext password to arraybuffer
+ 
+// Translate plaintext password to arraybuffer   
 function str2ab(str) {
-  let out = null;
-  let encoder = new TextEncoder();
-  out = encoder.encode(str);
-  return out;
+    let bout = null;
+    let bib = new TextEncoder();
+    bout = bib.encode(str);
+    return bout;
 }
